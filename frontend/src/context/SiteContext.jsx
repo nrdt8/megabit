f975bb1d8b6503ba596fd0c2cf80c6fc344e09b8
Amir Mansurov <m0820@mail.ru>
2026-04-25 15:41:13 +0500

    fix api url

--- frontend/src/context/SiteContext.jsx
+++ frontend/src/context/SiteContext.jsx
@@ -1,7 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-const API_DATA_URL = import.meta.env.VITE_DATA_API_URL;
-
 const SiteContext = createContext();
 
 export function SiteProvider({ children }) {
@@ -16,7 +14,7 @@ export function SiteProvider({ children }) {
   const [callback, setCallback] = useState(null);
 
   useEffect(() => {
-    fetch(`${API_DATA_URL}/api/data`)
+    fetch(`/api/data`)
       .then((r) => r.json())
       .then((data) => {
         setCompany(data.company);
